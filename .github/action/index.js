716313c49068cd714b25a83ec31aa28e43655faf
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-08-30 13:01:34 +1000

    Update index.js

--- .github/action/index.js
+++ .github/action/index.js
@@ -2,8 +2,8 @@ const fs = require("fs");
 
 const body = "## Project Title\r\nAxel White";
 
-fs.mkdirSync("project");
-fs.writeFile("./project/README.md", "MY README", c => {
+fs.mkdirSync("Axel White");
+fs.writeFile("./Axel White/README.md", "## Axel White", c => {
    if (c) return;
    console.log("Saved");
 });
